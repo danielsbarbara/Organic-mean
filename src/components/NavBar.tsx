a8042c4245a9e187b42a12288b55d2eb8ddfa5f6
Ana Isa Pereira Nogueira <139065337+AnaIsaPN@users.noreply.github.com>
2024-02-04 22:01:51 +0000

    language switch adjustment

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,22 +16,22 @@ export function NavBar() {
       {/* all the other elements */}
       <div className="pl-2 pr-2 md:p-4 bg-darkTransparent h-28 flex justify-evenly items-center md:justify-between ">
         <Logo text={toLocalTranslation("landing_page_title")} />
-        <div className="flex gap-2 self-end p-3 md:self-center">
-          <Link to={"/aboutme"}>
-            <NavItem description={toLocalTranslation("nav_bar_title_1")} />
-          </Link>
-          <Link to={"/products"}>
-            <NavItem description={toLocalTranslation("nav_bar_title_2")} />
-          </Link>
-          <Link to={"/contacts"}>
-            <NavItem description={toLocalTranslation("nav_bar_title_3")} />
-          </Link>
-          <div className="ml-3 mb-">
-            <LanguageSwitch/>
+        <div className="flex flex-col">
+          <div className="ml-3 self-end mr-2">
+            <LanguageSwitch />
           </div>
-                
+          <div className="flex gap-2 self-end justify-self-end p-3 md:self-center">
+            <Link to={"/aboutme"}>
+              <NavItem description={toLocalTranslation("nav_bar_title_1")} />
+            </Link>
+            <Link to={"/products"}>
+              <NavItem description={toLocalTranslation("nav_bar_title_2")} />
+            </Link>
+            <Link to={"/contacts"}>
+              <NavItem description={toLocalTranslation("nav_bar_title_3")} />
+            </Link>
+          </div>
         </div>
-  
       </div>
     </>
   );
