8ce03faf32ffe79255140e35fa71aabf000197a7
Ana Isa Pereira Nogueira <139065337+AnaIsaPN@users.noreply.github.com>
2024-02-21 19:32:46 +0000

    dropfilter fix

--- src/components/CollectionItem.tsx
+++ src/components/CollectionItem.tsx
@@ -9,14 +9,18 @@ interface CollectionItemProp{
 
 export const CollectionItem: React.FC<CollectionItemProp> = ({ description, iconPath, onClick, setFilter }) => {
    
-    const handleClick = (description:string) => {
-        if (description === "") return 
-        setFilter((prev: any) => ({...prev, category: description}))
+    const handleClick = () => {
+        if (onClick) {
+            onClick(); 
+        }
+        if (setFilter && description !== "") {
+            setFilter((prev: any) => ({...prev, category: description}));
+        }
     }
 
     return( 
         <>
-            <div className="p-1 flex flex-col items-center gap-1 md:text-xl cursor-pointer relative w-fit after:block after:content-[''] after:relative after:h-[1px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center" onClick={()=> handleClick(description)} >
+            <div className="p-1 flex flex-col items-center gap-1 md:text-xl cursor-pointer relative w-fit after:block after:content-[''] after:relative after:h-[1px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center" onClick={handleClick} >
                 {iconPath != 'null' && <img src={iconPath} className="w-9 md:w-10"/>} <p className="text-white text-xs md:text-base ">{description}</p>
             </div>
 
