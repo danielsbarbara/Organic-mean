f790617b1fe0ab0e790797eabbb7b09a3a5f36c5
Ana Isa Pereira Nogueira <139065337+AnaIsaPN@users.noreply.github.com>
2024-03-15 23:14:59 +0000

    test

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,7 @@ import { NavItem } from "./NavItem";
 import { toLocalTranslation } from "../i18n/toLocalTranslation";
 import { Link } from "react-router-dom";
 import { LanguageSwitch } from "./LanguageSwitch";
+import { FooterItem } from "./FooterItem";
 
 export function NavBar() {
   const navItemArr = [
@@ -17,7 +18,12 @@ export function NavBar() {
       <div className="pl-2 pr-2 md:p-4 bg-darkTransparent h-28 flex justify-evenly items-center md:justify-between ">
         <Logo text={toLocalTranslation("landing_page_title")} />
         <div className="flex flex-col">
-          <div className="mb-2 self-end mr-2">
+          <div className="flex mb-2 self-end mr-2 gap-6">
+            <div className="flex flex-col items-center mt-1.5 hover:scale-110 transition-transform duration-150 ease-in-out">
+              <Link to={"/FAQPage"}>
+                <FooterItem title="FAQ" />
+              </Link>
+            </div>
             <LanguageSwitch />
           </div>
           <div className="flex gap-2 self-end justify-self-end p-2 md:self-center">
